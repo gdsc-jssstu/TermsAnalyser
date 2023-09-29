4c55297ecb055a5675eef135dd132923b133ef58
avinash84319 <avichinnu22@gmail.com>
2023-09-29 23:16:22 +0530

    adding readme -avinash

---
+++ MLapi/app.py
@@ -0,0 +1,181 @@
+# Description: This file contains the code for the server side of the application.
+
+# Importing the required libraries.(dont change anything here)----------------------------------------------------------------------------------------------------
+from flask import Flask
+API_TOKEN="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"
+API_URL = "https://api-inference.huggingface.co/models/remzicam/privacy_intent"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}  
+from transformers import pipeline
+import requests
+summarizer = pipeline("summarization", model="knkarthick/MEETING_SUMMARY")
+from pdfminer.high_level import extract_text
+from sentence_transformers import SentenceTransformer, util,CrossEncoder
+# from text_generation import Client
+# Importing the required libraries.-------------------------------------------------------------------------------------------------------------------------------
+
+
+#chat (dont change anything here)--------------------------------------------------------------------------------------------------------------------------------------
+def embed(fname,window_size,step_size):
+    text=extract_text(fname)
+    text=" ".join(text.split())
+    text_tokens=text.split()
+
+    sentences = []
+    for i in range(0, len(text_tokens), step_size):
+        window=text_tokens[i:i+window_size]
+        if len(window)<window_size:
+            break
+        sentences.append(window)
+
+    paragraphs=[" ".join(s) for s in sentences]
+
+    model=SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    model.max_seq_length=512
+    cross_encoder=CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
+
+    embeddings=model.encode(paragraphs,show_progress_bar=True,convert_to_tensor=True)
+    return model,cross_encoder,embeddings,paragraphs
+def search(query,model,cross_encoder,embeddings,paragraphs,top_k=32):
+    query_embedding=model.encode(query,convert_to_tensor=True)
+    # query_embedding=query_embedding.cuda()
+    hits=util.semantic_search(query_embedding,embeddings,top_k=top_k)[0]
+
+    cross_input=[[query,paragraphs[hit['corpus_id']]] for hit in hits]
+    cross_scores=cross_encoder.predict(cross_input)
+
+    for idx in range(len(hits)):
+        hits[idx]['cross_score']=cross_scores[idx]
+
+    results=[]
+    hits=sorted(hits,key=lambda hit:hit['cross_score'],reverse=True)
+    for hit in hits[:5]:
+        results.append(paragraphs[hit['corpus_id']].replace("\n"," "))
+    return results
+
+#chat ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+app=Flask(__name__)
+
+
+#routes------------------------------------------------------------------------------------------------------------------------------------------------------------
+@app.route("/")  #route for home page
+def home():
+    return "Welcome to the api"
+
+@app.route("/summarypdf")  #route for summarizing pdf
+def summarize():
+    try:
+        data=extract_text('data.pdf')     #extracting text from pdf
+    except:
+        return "Pdf not uploaded"
+    ans=[]
+    n=len(data)/1024          #splitting the text into 1024 word chunks
+    for i in range(n):
+        ans.append(summarizer(data[i*1024:(i+1)*1024]))
+    
+    ansstr=""          #combining the chunks into a single string
+    for i in ans:
+        ansstr+=i[0]['summary_text']
+    return ansstr         #returning the summary
+
+@app.route("/summaryfor=<data>")                            #route for summarizing text
+def summarizefor(data):
+    data=data.split('.')                          #splitting the text into sentences
+    data={'inputs':data}                   #creating a json object
+    response = requests.post(API_URL, headers=headers, json=data)             #sending the json object to the api
+    return response.json()                         #returning the response
+
+@app.route("/classifierpdf")
+def classifypdf():                                #route for classifying pdf
+    try:
+        data=extract_text('data.pdf')                       #extracting text from pdf
+    except:
+        return {'error':"Pdf not uploaded"}
+    data=data.split('.')
+    sdata={'inputs':data}
+    response = requests.post(API_URL, headers=headers, json=sdata)               #sending the json object to the api
+
+    store=[]                                                         #storing the labels
+    for i in response.json():
+        for j in i:
+            if j['score']>0.5:
+                store.append(j['label'])
+
+    dsrd=[]                                               #storing the data according to the labels
+    dcu=[]
+    dsp=[]
+    dsd=[]
+    other=[]
+
+    for i in range(len(store)):                                   #segregating the data according to the labels
+        if store[i]=='data-storage-retention-deletion':
+            dsrd.append(data[i])
+        elif store[i]=='data-collection-usage':
+            dcu.append(data[i])
+        elif store[i]=='data-security-protection':
+            dsp.append(data[i])
+        elif store[i]=='data-sharing-disclosure':
+            dsd.append(data[i])
+        else:
+            other.append(data[i])                                     #returning the data
+
+    
+
+    return {"data-storage-retention-deletion":dsrd,"data_collection_usage":dcu,"data-security-protection":dsp,"data-sharing-disclosure":dsd,"other":other}
+
+
+@app.route("/classifierfor=<data>")
+def classify(data):                                        #route for classifying text
+    classdata=data.split('.')
+    classdata={'inputs':classdata}                     #creating a json object
+
+    response = requests.post(API_URL, headers=headers, json=classdata)            #sending the json object to the api
+
+    store=[]                                                         #storing the labels
+    for i in response.json():
+        for j in i:
+            if j['score']>0.5:
+                store.append(j['label'])
+
+    dsrd=[]                                               #storing the data according to the labels
+    dcu=[]
+    dsp=[]
+    dsd=[]
+    other=[]
+
+    for i in range(len(store)):                                   #segregating the data according to the labels
+        if store[i]=='data-storage-retention-deletion':
+            dsrd.append(data[i])
+        elif store[i]=='data-collection-usage':
+            dcu.append(data[i])
+        elif store[i]=='data-security-protection':
+            dsp.append(data[i])
+        elif store[i]=='data-sharing-disclosure':
+            dsd.append(data[i])
+        else:
+            other.append(data[i])
+
+    return {"data-storage-retention-deletion":dsrd,"data_collection_usage":dcu,"data-security-protection":dsp,"data-sharing-disclosure":dsd,"other":other}
+
+
+@app.route("/chatfor=<data>")
+def chat(data):                                       #route for chatbot
+    try:
+        model,cross_encoder,embeddings,paragraphs=embed('data.pdf',129,100)          #extracting text from pdf
+    except:
+        return "Pdf not uploaded"
+    results=search(data,model,cross_encoder,embeddings,paragraphs,32)               #searching for the query
+    return results[0]                                       #returning the response
+
+
+@app.route("/pdfu",methods=['POST'])
+def pdfu():                                                    #route for uploading pdf
+    pdf=requests.files['pdf']                                    #extracting the pdf
+    try:
+        pdf.save('data.pdf')                                     #saving the pdf
+        return "success"                                                    #returning success
+    except:
+        return "Pdf not uploaded"
+
+#routes------------------------------------------------------------------------------------------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# TermsAnalyser
+# TermsAnalyser 📃⚖️
+
+## Project Structer :-
+
+The Terms and Conditions Analysis App is a Flutter application designed to analyze terms and conditions documents of various apps and provide users with valuable insights. The app employs natural language processing (NLP) techniques to extract summaries, identify problematic statements, and enable users to ask questions about the terms and conditions of different services and applications.
+
+Key Features:
+
+Document Analysis: The app can analyze terms and conditions documents in formats, including PDFs, text .
+Summarization: It generates concise summaries of lengthy terms and conditions documents, making it easier for users to understand their contents.
+Problematic Statement Detection: The app uses NLP to identify potentially problematic or unusual clauses within the terms and conditions, highlighting areas that may require further attention.
+Question-Answering: Users can ask questions about specific sections or clauses in the terms and conditions, and the app provides relevant answers, enhancing user comprehension.
+
+## Tech stack used:-
+
+- Python 3.11.3
+- Flask 2.3.3
+
+## Project Setup:-
+
+```
+git clone https://github.com/gdsc-jssstu/TermsAnalyser.git
+
+```
+
+### Flutter part:-
+
+
+### ML part:-
+
+After making sure you have python 3.11.3 installed on your system,if not then install it from [here](https://www.python.org/downloads/)
+
+Now change the directory to MLapi by running the following command in the terminal
+
+```
+cd MLapi
+
+```
+
+Now install all the dependencies by running the following command in the terminal
+
+```
+pip install -r requirements.txt
+
+```
+
+This might take a while to install all the dependencies
+
+
+Now run the following command in the terminal to start the server
+Make sure you are in the MLapi directory
+
+```
+flask --app app run
+
+```
+First time running this command might take a while as it will download the models and store them in the cache folder pls be patient
+
+Now your server is up and running on port 5000 and the output in terminal should look like this
+
+```
+PS D:\TermsAnalyser\MLapi> flask --app app run
+ * Serving Flask app 'app'
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+
+```
+
+Now you can test the api by sending a post request to the following url
+
+```
+http://127.0.0.1:5000
+
+``` 
+
+
+
+
